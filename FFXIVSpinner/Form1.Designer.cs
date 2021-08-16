b65184b68f9974c0a98fae2080a42b2fadf9311a
chris <cstaudigel@gmail.com>
2021-08-15 21:20:30 -0400

    Add project files.

---
+++ FFXIVSpinner/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace FFXIVSpinner
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelProcess = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelProcess
+            // 
+            this.labelProcess.AutoSize = true;
+            this.labelProcess.Location = new System.Drawing.Point(12, 69);
+            this.labelProcess.Name = "labelProcess";
+            this.labelProcess.Size = new System.Drawing.Size(101, 15);
+            this.labelProcess.TabIndex = 0;
+            this.labelProcess.Text = "Process: {Process}";
+            this.labelProcess.Click += new System.EventHandler(this.labelProcess_Click);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(12, 34);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(222, 23);
+            this.buttonStart.TabIndex = 3;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(211, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Holds the A button until you click stop";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(246, 93);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.labelProcess);
+            this.Name = "Form1";
+            this.Text = "FFXIV Spinner";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelProcess;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ FFXIVSpinner/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace FFXIVSpinner
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        static extern bool PostMessage(IntPtr hWnd, UInt32 Msg, int wParam, int lParam);
+
+
+        const UInt32 WM_KEYDOWN = 0x0100;
+        const UInt32 WM_KEYUP = 0x0101;
+
+        const int VK_A = 0x41;
+
+        private Process _process { get; set; }
+        private bool _running { get; set; }
+
+        private Keys _key { get; set; }
+
+        public Form1()
+        {
+            _running = false;
+            InitializeComponent();
+        }
+
+        private void labelProcess_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _process = FindProcess();
+
+                labelProcess.Text = $"Process: {_process.ProcessName}({_process.Id})";
+            } 
+            catch
+            {
+                MessageBox.Show("Cannot hook Final Fantasy XIV Process");
+            }
+        }
+
+        private Process FindProcess()
+        {
+            Process proc = Process.GetProcesses().FirstOrDefault(p => p.ProcessName.StartsWith("ffxiv", StringComparison.CurrentCultureIgnoreCase));
+
+            if (null == proc)
+            {
+                throw new Exception("cannot find process");
+            }
+
+            return proc;
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            _running = !_running;
+
+            if (!_running)
+            {
+                buttonStart.Text = "Stop";
+
+                PostMessage(_process.MainWindowHandle, WM_KEYDOWN, VK_A, 0);
+            } 
+            else
+            {
+                buttonStart.Text = "Start";
+                PostMessage(_process.MainWindowHandle, WM_KEYUP, VK_A, 0);
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            labelProcess_Click(sender, e);
+        }
+    }
+}
